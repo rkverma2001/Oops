dfce6f396f7cfe2990cd4c047b2f059af7c2cc3b
Ritesh Kumar <riteshkumarverma2018@gmail.com>
2023-05-22 22:22:59 +0530

    Absstraction completed

--- Abstractionex.java
+++ Abstractionex.java
@@ -1,16 +1,26 @@
 public class Abstractionex {
     public static void main(String[] args) {
-        Horse h = new Horse();
-        h.eat();
-        h.walk();
+        // Horse h = new Horse();
+        // h.eat();
+        // h.walk();
+        // System.out.println(h.color);
 
-        Chicken c = new Chicken();
-        c.eat();
-        c.walk();
+        // Chicken c = new Chicken();
+        // c.eat();
+        // c.walk();
+
+        Mustang myHorse = new Mustang();
+        // Animal -> Horse -> Mustang
     }
 }
 
 abstract class Animal {
+    String color;
+
+    Animal() {
+        System.out.println("animal constructor called");
+    }
+
     void eat() {
         System.out.println("animal eats");
     }
@@ -19,12 +29,30 @@ abstract class Animal {
 }
 
 class Horse extends Animal {
+    Horse() {
+        System.out.println("Horse constructor called");
+    }
+
+    void changeColor() {
+        color = "dark brown";
+    }
+
     void walk() {
         System.out.println("walks on 4 legs");
     }
 }
 
+class Mustang extends Horse {
+    Mustang() {
+        System.out.println("Mustang constructor");
+    }
+}
+
 class Chicken extends Animal {
+    void changeColor() {
+        color = "yellow";
+    }
+
     void walk() {
         System.out.println("walks on 2 legs");
     }
